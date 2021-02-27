0415a3eef3ab4d7ca54e34c9fe815b59c243e5c9
Gelnaz Abdreeva <gelnasotschek@mail.ru>
2021-02-28 01:49:22 +0400

    with sort

--- src/main/java/collection/Main.java
+++ src/main/java/collection/Main.java
@@ -4,6 +4,57 @@ import collection.impl.MyCollection;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        MyCollection<Integer> intCollection = new MyCollection<>();
+        System.out.println("Size before add something " + intCollection.size());
+        intCollection.add(13);
+        intCollection.add(15);
+        intCollection.add(17);
+        intCollection.add(19);
+        intCollection.add(16);
+        intCollection.add(15);
+        MyCollection<Integer> forAdd = new MyCollection<>();
+        forAdd.add(1);
+        forAdd.add(2);
+        forAdd.add(3);
+        forAdd.add(15);
+        forAdd.add(5);
+        intCollection.addAll(forAdd);
+        System.out.println("Size after add something " +intCollection.size());
+        System.out.println("intCollection");
+        intCollection.print();
+        intCollection.insert(5, 89);
+        System.out.println("Element number 5 = " + intCollection.get(5).get());
+        intCollection.remove(7);
+        System.out.println("intCollection after remove and insert");
+        intCollection.print();
+        System.out.println("the first occurrence of a number 15 in intCollection " + intCollection.first(15));
+        System.out.println("the last occurrence of a number 15 in intCollection " + intCollection.last(15));
+        System.out.println("contains 15? " + intCollection.contains(15));
+        System.out.println("isEmpty? " + intCollection.isEmpty());
+
+        AdvancedList<Integer> newCollection = intCollection.shuffle();
+        System.out.println("shuffle");
+        for (int i = 0; i < newCollection.size(); i++){
+            System.out.print(newCollection.get(i).get() + " ");
+        }
+        System.out.println();
+
+        intCollection.print();
+
+        MyCollection<Integer> toSort = (MyCollection<Integer>) intCollection.sort(Integer::compareTo);
+
+        toSort.print();
+        intCollection.print();
+
+        MyCollection<String> strCollection = new MyCollection<>();
+        strCollection.add("qwerty");
+        strCollection.add("abc");
+        strCollection.add("dfg");
+        strCollection.add("ty764@");
+
+        MyCollection<String> newStrCollection = (MyCollection<String>) strCollection.sort(String::compareTo);
+        newStrCollection.print();
+        strCollection.print();
 
     }
 }
--- src/main/java/collection/impl/MyCollection.java
+++ src/main/java/collection/impl/MyCollection.java
@@ -5,8 +5,8 @@ import collection.AuthorHolder;
 import collection.SimpleList;
 
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.Optional;
+import java.util.Random;
 
 public class MyCollection<T> implements AdvancedList<T>, AuthorHolder {
 
@@ -133,13 +133,79 @@ public class MyCollection<T> implements AdvancedList<T>, AuthorHolder {
     @Override
     public AdvancedList<T> shuffle() {
         AdvancedList<T> newContainer = new MyCollection<T>();
+        final T[] interim =  container.clone();
+        for (int i = 0; i < numberItems; i++){interim[i] = container[i];}
+        int interemSize = numberItems;
+        int index;
+        Random random = new Random();
+        for (int i = 0; i < interemSize; i++){
+            index = numberItems != 1 ? random.nextInt(numberItems - 1) : 0;
+            newContainer.add(container[index]);
+            try {
+                remove(index);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
 
-        return null;
+        for (int i = 0; i < interemSize; i++){add(interim[i]);}
+        return newContainer;
     }
 
     @Override
     public AdvancedList<T> sort(Comparator<T> comparator) {
-        return null;
+        AdvancedList<T> sortedList = new MyCollection<T>();
+
+        T[] containerToSort = container.clone();
+        quickSort(containerToSort, 0, numberItems - 1, comparator);
+
+        for (int i = 0; i < numberItems; i++){
+            sortedList.add(containerToSort[i]);
+        }
+
+        return sortedList;
     }
 
+    private void quickSort(T[] container, int first, int last, Comparator<T> comparator) {
+        T pivot = container[first];
+        int pivotN;
+        int partLeft = first;
+        int partRight = last;
+
+        while(partLeft < partRight){
+
+            while (comparator.compare(container[partRight], pivot) >= 0 && partLeft < partRight){
+                partRight--;
+            }
+            if (partLeft != partRight) {
+                container[partLeft] = container[partRight];
+            }
+
+            while (comparator.compare(container[partLeft], pivot) <= 0 && partLeft < partRight){
+                partLeft++;
+            }
+            if (partLeft != partRight){
+                container[partRight] = container[partLeft];
+                partRight--;
+            }
+        }
+
+        container[partLeft] = pivot;
+        pivotN = partLeft;
+        partLeft = first;
+        partRight = last;
+        if(pivotN > partLeft) {
+            quickSort(container, partLeft, pivotN -1, comparator);
+        }
+        if(partRight > pivotN) {
+            quickSort(container, pivotN + 1, partRight, comparator);
+        }
+    }
+
+    public void print(){
+        for (int i = 0; i < numberItems; i++){
+            System.out.print(container[i] + " ");
+        }
+        System.out.println();
+    }
 }
